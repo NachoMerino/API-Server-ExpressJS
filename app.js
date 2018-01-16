37ea63c57ddc210cf9c2e86afbc47eef696b16ce
Nacho Merino <escatman@gmail.com>
2018-01-16 23:49:11 +0100

    new changes

--- app.js
+++ app.js
@@ -7,24 +7,36 @@ const os = require('os');
 const osu = require('os-utils');
 const bodyParser = require('body-parser');
 const app = express();
+const bcrypt = require('bcrypt');
 const Router = express.Router;
 
 const port = 9090;
 
+let osFolder = process.env.HOME + '/.status_login';
+let statusConfig = null;
+if (!fs.existsSync(osFolder)) {
+  fs.mkdirSync(osFolder);
+  let initialConfig = {
+    mysql_user: '',
+    mysql_db: '',
+    mysql_pwd: '',
+  }
+  fs.writeFileSync(osFolder + '/.config.json', JSON.stringify(initialConfig));
+  console.log('The config folder does not exist, it has been created now. The server will exit now');
+  process.exit();
+
+} else {
+  statusConfig = require(osFolder + '/.config.json');
+}
+
+// you need to access /home/"yourUserName"/.status_login/.config.json and set the DB data
 const con = mysql.createConnection({
   host: 'localhost',
-  user: 'nacho',
-  password: 'qwerty',
-  database: 'online_shop'
+  user: statusConfig.mysql_user,
+  password: statusConfig.mysql_pwd,
+  database: statusConfig.mysql_db,
 });
 
-//DB for Status Login
-const conToStatus = mysql.createConnection({
-  host: 'localhost',
-  user: 'nacho',
-  password: 'qwerty',
-  database: 'status_login'
-});
 
 // diskUsage in Bytes (1 Byte 1e-9 GB)
 const disk = require('diskusage');
@@ -119,7 +131,7 @@ Object.keys(ifaces).forEach((ifname) => {
       // this interface has only one ipv4 adress
       console.info(`The IP address is: ${iface.address} connected via ${ifname} `);
       app.get('/', (req, res) => {
-        res.render('index', {
+        res.render('login-Screen', {
           serverIP: iface.address,
           hostName: os.hostname(),
         });
@@ -134,156 +146,9 @@ Object.keys(ifaces).forEach((ifname) => {
     ++alias;
   });
 });
-///Conect to MySQL
-apiRouter.get('/products', (req, res) => {
-  con.query('select * from products', (err, rows) => {
-    if (err) {
-      throw err;
-    } else {
-      res.type('json');
-      res.send(rows);
-    }
-  });
-});
 
-apiRouter.get('/categories', (req, res) => {
-  con.query('select * from product_categories', (err, rows) => {
-    if (err) {
-      throw err;
-    } else {
-      res.type('json');
-      res.send(rows);
-    }
-  });
-});
-///MySQL END
-apiRouter.get('/activecustomers', (req, res) => {
-  con.query('select id from customers where active = 1 ', (err, rows) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(rows);
-    }
-  });
-});
 
-apiRouter.get('/customers/:userid', (req, res) => {
-  con.query('select * from customers where active = 1 and id = ?', [req.params.userid], (err, rows) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(rows);
-    }
-  });
-});
 
-apiRouter.get('/payment_methods', (req, res) => {
-  con.query('SELECT * FROM payment_methods', (err, rows) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(rows);
-    }
-  });
-});
-
-apiRouter.put('/activate/:userid', (req, res) => {
-  con.query('UPDATE customers set active = ? where id = ?', [req.body.status, req.params.userid],
-    (err, rows) => {
-      if (err)
-        throw err;
-      res.json(rows);
-    });
-});
-// postUser.sh
-apiRouter.post('/user', (req, res) => {
-
-  con.query('select email from customers where email = ?', [req.body.email],
-    function(err, rows) {
-      if (err)
-        throw res.json();
-      if (rows.length > 0) {
-        res.json('The email ' + [req.body.email] + ' its already in use.');
-      } else {
-        con.query('INSERT INTO customers (firstname,lastname,birthdate,phone,city,street,postal,email) VALUES (?,?,?,?,?,?,?,?)', [req.body.firstname,
-            req.body.lastname,
-            req.body.birthdate,
-            req.body.phone,
-            req.body.city,
-            req.body.street,
-            req.body.postal,
-            req.body.email
-          ],
-          (err, rows) => {
-            if (err) {
-              throw err;
-            } else {
-              res.json(rows);
-            }
-          });
-      }
-    });
-});
-//postOrder.sh
-apiRouter.post('/order', (req, res) => {
-  /*
-    fs.writeFile(path.resolve(__dirname, './../../orders/orders'+Date.now()+'.txt'), JSON.stringify(req.body),
-      (err)=>{
-        if(err)
-          res.json({error: err});
-        res.json({success:'order saved'})
-      });
-  */
-  con.query('INSERT INTO orders (customer_id,created,payment_method_id) VALUES (?,now(),?)', [req.body.user.customer_id, req.body.payment_method_id],
-    (err, rows) => {
-      if (err) {
-        throw err;
-      } else {
-        con.query('INSERT INTO order_details (order_id,product_id,price) VALUES (' + rows.insertId + ',?,?)', [req.body.products[0].id, req.body.products[0].price],
-          (err, rows) => {
-            if (err) {
-              throw err;
-            } else {
-              res.json(rows);
-            }
-          });
-      }
-    });
-});
-// modifyUser.sh
-apiRouter.put('/user/:userid', (req, res) => {
-  var sql = 'UPDATE customers set ';
-  // console.info('user id: ', req.params.userid);
-  var i = 1;
-  var bodyLength = Object.keys(req.body).length;
-  var values = [];
-  for (var field in req.body) {
-    sql += field + ' = ?';
-    if (i < bodyLength)
-      sql += ',';
-    i++;
-    values.push(req.body[field]);
-    // console.info('field is:',field);
-    // console.info('value is:',req.body[field]);
-  }
-  sql += 'where id = ?';
-  values.push(req.params.userid);
-  con.query(sql, values,
-    (err, rows) => {
-      if (err)
-        throw err;
-      res.json(rows);
-    });
-});
-
-apiRouter.delete('/delete/:userid', (req, res) => {
-  con.query('UPDATE customers set deleted = now() where id = ?', [req.params.userid],
-    (err, rows) => {
-      if (err)
-        throw err;
-      res.json(rows);
-    });
-});
 // Redirecting a 404 Error
 app.get("*", (req, res) => {
   res.render('404', {
